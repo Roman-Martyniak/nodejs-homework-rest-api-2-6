bc25d5bce8ed11a5bf5e633c7fa28640fea8bf93
Roman Martyniak <110495171+Roman-Martyniak@users.noreply.github.com>
2023-06-07 23:30:15 +0300

    fix3

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,7 @@ const validateBody = (schema) => {
     const validate = (req, res, next) => {
         const { error } = schema.validate(req.body);
         if (error) {
-            return next(httpError(400, `missing required name field ${error.message}`));
+            return next(httpError(400, `missing required name field `));
         }
 
         next();
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,6 +2,7 @@ const express = require('express')
 const ctrl = require("../../controllers/contacts");
 const router = express.Router()
 
+const ctrlWrapper = require('../../helpers/ctrlWrapper')
 const validateEmptyBody = require('../../middlewares/validateEmptyBody')
 const validateBody = require('../../middlewares/validateBody');
 const schema = require('../../schemas/schema')
@@ -10,10 +11,10 @@ router.get("/", ctrl.getListContacts);
 
 router.get("/:contactId", ctrl.getContacts);
 
-router.post("/", validateBody(schema), ctrl.postAddContact);
+router.post("/",validateEmptyBody, validateBody(schema), ctrlWrapper(ctrl.postAddContact));
 
 router.delete("/:contactId", ctrl.deleteContact);
 
-router.put("/:contactId",validateEmptyBody, validateBody(schema), ctrl.putUpdateContact);
+router.put("/:contactId",validateEmptyBody, validateBody(schema), ctrlWrapper(ctrl.putUpdateContact));
 
 module.exports = router;
