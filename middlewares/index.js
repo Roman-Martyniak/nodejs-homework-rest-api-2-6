68dbe80d2e54fa7ec2442439c532e072e74d4d88
Roman Martyniak <110495171+Roman-Martyniak@users.noreply.github.com>
2023-07-01 15:18:45 +0300

    commit

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,7 @@ const isValidId = require("./isValidId");
 const validateBody = require("./validateBody");
 const validateEmptyBody = require("./validateEmptyBody");
 const authenticate = require("./authenticate");
-const isValidUserId = require('./isValidUserId')
+const isValidUserId = require('./isValidUserId');
+const upload = require('./upload')
 
-module.exports = { validateBody, validateEmptyBody, isValidId, authenticate, isValidUserId };
+module.exports = { validateBody, validateEmptyBody, isValidId, authenticate, isValidUserId,upload };
